ac6b3c82cfb9e75cfc9f3f6187f38bc4a5aa9e54
Alex <alexsanty10lm@gmail.com>
2024-10-27 22:47:08 -0500

    Se añadió una ruta de prueba para register

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import RegistrationForm from './sign-in/page/sign-in'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Router>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,6 +31,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Routes>
+        <Route path="/register" element={<RegistrationForm />} />
+      </Routes>
+      </Router>
     </>
   )
 }
