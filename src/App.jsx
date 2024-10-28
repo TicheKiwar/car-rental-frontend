6a6f0c22d8b6b9622665d79233d453868349c01c
Alex <alexsanty10lm@gmail.com>
2024-10-28 11:30:00 -0500

    Se modificaron las rutas para añadir el path de registro

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import RegistrationForm from "./sign-in/page/sign-in";
 import Login from "./login/login";
 
 function App() {
@@ -9,8 +10,10 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/register" element={<RegistrationForm />} />
       </Routes>
     </Router>
   );
+}
 
 export default App;
