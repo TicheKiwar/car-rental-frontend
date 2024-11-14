ac5af5d1f7614b54bebb994cd6c7b0c9116bd14f
Alex <alexsanty10lm@gmail.com>
2024-11-14 03:26:26 -0500

    Se modificó el registro de usuarios para permitir solo el registro de clientes

--- src/sign-in/page/sign-in.tsx
+++ src/sign-in/page/sign-in.tsx
@@ -1,31 +1,18 @@
-import React, { useState } from 'react';
-import { Form, Button, Radio, message, Row, Col } from 'antd';
-import { createClient, createEmployee } from '../../services/sign-in.service';
-import ClientFields from '../components/ClientFields';
-import EmployeeFields from '../components/EmployeeFields';
+import React from 'react';
+import { Form, Button, message } from 'antd';
+import { createClient } from '../../services/sign-in.service';
 import CommonFields from '../components/commonFields';
 import { useNavigate } from 'react-router-dom';
+import ClientFields from '../components/clientFields';
 
 const RegistrationForm = () => {
   const [form] = Form.useForm();
-  const [userType, setUserType] = useState('cliente');
   const navigate = useNavigate();
-
-  const handleUserTypeChange = (e) => {
-    setUserType(e.target.value);
-    form.resetFields();
-  };
 
   const onFinish = async (values) => {
     try {
-      const userData = { ...values, rol: values.rol?.toLowerCase() };
-
-      if (userType === 'empleado') {
-        await createEmployee(userData);
-      } else {
-        await createClient(userData);
-      }
-
+      const userData = { ...values};
+      await createClient(userData);
       message.success('Registro exitoso');
       form.resetFields();
       navigate('/');
@@ -38,16 +25,8 @@ const RegistrationForm = () => {
     <div style={{ maxWidth: 600, margin: '0 auto', padding: '2rem' }}>
       <h2>Registro</h2>
       <Form form={form} layout="vertical" onFinish={onFinish}>
-      <Form.Item label={<strong>Registrarse como: </strong>}>
-          <Radio.Group onChange={handleUserTypeChange} value={userType}>
-            <Radio value="cliente">Cliente</Radio>
-            <Radio value="empleado">Empleado</Radio>
-          </Radio.Group>
-        </Form.Item>
         <CommonFields />
-        {userType === 'cliente' && <ClientFields />}
-        {userType === 'empleado' && <EmployeeFields />}
-
+        <ClientFields />
         <Form.Item>
           <Button type="primary" htmlType="submit" block>
             Registrarse
