83b56796ae465d66ee8e048855d64502be97141d
Alex <alexsanty10lm@gmail.com>
2024-11-20 22:06:41 -0500

    Se modificarón las rutas en App

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RegistrationForm from "./sign-in/page/sign-in";
-import Login from "./login/login";
-
+import Login from "./auth/login";
+import ForgotPassword from "./auth/forgotPassword";
+import ResetPassword from "./auth/resetPassword";
 function App() {
-
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Login/>} />
         <Route path="/register" element={<RegistrationForm />} />
+        <Route path="/password_reset" element={<ForgotPassword/>} />
+        <Route path="/reset_password/:token" element={<ResetPassword/>} />
+
       </Routes>
     </Router>
   );
