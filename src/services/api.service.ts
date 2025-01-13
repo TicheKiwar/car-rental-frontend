bac62a5eb6e17bf58d7b953505290b600fa24314
TicheKiwar <kiwartiche@gmail.com>
2025-01-13 13:54:15 +0000

    redirrecionamiento

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 // export const url = "http://192.168.231.130:3000"
-export const url = "http://192.168.231.130:3000"
+export const url = "http://192.168.1.112:3000"
 export const api = axios.create({
     baseURL: url,
     headers: {
