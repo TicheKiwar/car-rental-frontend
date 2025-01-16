0f17c79f61b3950c64f48cd596ef33527096218a
TicheKiwar <140968057+TicheKiwar@users.noreply.github.com>
2025-01-16 11:39:56 -0500

    Update api.service.ts

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 // export const url = "http://192.168.231.130:3000"
-export const url = "http://backend:3000"
+const isDocker = process.env.REACT_APP_DOCKER_ENV === "true";
+export const url = isDocker ? "http://backend:3000" : "http://192.168.231.130:3000";
+
 export const api = axios.create({
     baseURL: url,
     headers: {
